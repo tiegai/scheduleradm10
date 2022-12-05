801a5db8a1f0070ba8df8ea8d02c30b226cf8268
jch527_nike <Jerry.Chen3@nike.com>
2022-12-05 10:17:04 +0800

    sql

--- db/nike_scheduler_job.sql
+++ db/nike_scheduler_job.sql
@@ -118,7 +118,7 @@ CREATE TABLE `xxl_job_lock` (
   PRIMARY KEY (`lock_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `xxl_job_group`(`id`, `app_name`, `title`, `address_type`, `address_list`, `update_time`) VALUES (1, 'xxl-job-executor-sample', '示例执行器', 0, NULL, '2022-11-03 22:21:31' );
+INSERT INTO `xxl_job_group`(`id`, `app_name`, `title`, `address_type`, `address_list`, `update_time`) VALUES (1, 'ncp-scheduler-executor', 'nike执行器', 0, NULL, '2022-11-03 22:21:31' );
 INSERT INTO `xxl_job_user`(`id`, `username`, `password`, `role`, `permission`) VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 1, NULL);
 INSERT INTO `xxl_job_lock` ( `lock_name`) VALUES ( 'schedule_lock');
 INSERT INTO `xxl_job_info` (`id`,`id_group`,`job_group`,`job_desc`,`add_time`,`update_time`,`author`,`alarm_email`,`schedule_type`,`schedule_conf`,`misfire_strategy`,`executor_route_strategy`,`executor_handler`,`executor_param`,`executor_block_strategy`,`executor_timeout`,`executor_fail_retry_count`,`glue_type`,`glue_source`,`glue_remark`,`glue_updatetime`,`child_jobid`,`trigger_status`,`trigger_last_time`,`trigger_next_time`,`trigger_start_time`,`trigger_end_time`) VALUES (1,0,0,'启动-到点<非人工关闭>所有任务','2022-11-08 17:13:33','2022-11-09 16:53:01','jerry','','CRON','* * * * * ?','DO_NOTHING','FIRST','basicHttpJobHandler','url:http://127.0.0.1:8080/xxl-job-admin/jobinfo/autoStartJobs\r\nmethod:GET\r\n','SERIAL_EXECUTION',0,0,'BEAN','','GLUE代码初始化','2022-11-08 17:13:33','',1,1669975559000,1669975560000,NULL,NULL);

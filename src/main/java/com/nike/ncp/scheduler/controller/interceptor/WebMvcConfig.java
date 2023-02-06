0cd19e400e0522a6f1cfff7ec971f4fc5f5432ac
jch527_nike <Jerry.Chen3@nike.com>
2023-02-06 11:38:23 +0800

    swagger permission

--- src/main/java/com/nike/ncp/scheduler/controller/interceptor/WebMvcConfig.java
+++ src/main/java/com/nike/ncp/scheduler/controller/interceptor/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(permissionInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(permissionInterceptor).addPathPatterns("/**").excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
         registry.addInterceptor(cookieInterceptor).addPathPatterns("/**");
     }
 }
